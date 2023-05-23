6c55a558cb2e276c62dae5f92b49421415ac8a5e
Makimaki1304 <23e1304@andrew.ac.jp>
2023-05-23 19:51:42 +0900

    feat: conflict resolve

--- app/src/App.js
+++ app/src/App.js
@@ -1,42 +1,65 @@
-import logo from './logo.svg';
+import logo from './Simarin.jpg';
 import './App.css';
-import HogeHoge from './Hoge.js'
-
-function Hoge() {
-  return (
-    <div>
-      <h1>Hoge</h1>
-    </div>
-  )
-}
+import{ useState }from "react";
 
 function App() {
+  const[counter,setCounter]=useState(0);
+  const plusOne=()=>{
+    setCounter(counter+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        {(function () {
-          const h1s = [];
-          for (let i = 0; i < 5; i++) {
-            h1s.push(<HogeHoge />);
-          }
-          return <div>{h1s}</div>;
-        }())}
-        <Hoge />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <header className="App-header">
+      <h1>Welcome to My page!</h1>
+    <img src={logo} className="App-logo" alt="logo" />
+     <div className="header-logo">Makimaki</div>
+     <div className="geta">日々進化するゲッター線に選ばれしもの</div>
       </header>
+      <body>
+      <p id="tabcontrol">
+      <div className="main-logo">
+    <div className="main-logo2">
+   <a href="#tabpage1">NEWS</a>
+   </div>
+   <div className="main-logo2">
+   <a href="#tabpage2">BIOGRAPHY</a>
+   </div>
+   <div className="main-logo2">
+   <a href="#tabpage3">DISCOGRAPHY</a>
+   </div>
+   </div>
+</p>
+<div id="tabbody">
+  <div className="news">
+   <div id="tabpage1">NEWS</div>
+   </div>
+      <div className="news-logo">2023.05.14 初メロディ完成！</div>
+      <div className="news-logo">2023.05.13 冷蔵餃子を冷蔵のまま食べてお腹クラッシュ！</div>
+      <div className="news-logo">2023.04.24 新作小説製作開始！</div>
+      <div className="news-logo">2023.03.01 アーマードコア for anser プレイ開始！</div>
+   <div className="news">
+    <div id="tabpage2">BIOGRAPHY</div>
     </div>
+    <div className="bio">
+      2000年代生まれ。日々をなんとなく生きるオタク。浅く広く深い知識でアニメを堪能する。
+      高校時代に小説執筆に挑戦するができたのは片手で数える程度。最近になってまた書き始めた。
+      音楽の勉強も始めたが脳のOSが追い付かない。ロングスリーパー、寝坊癖あり。
+      人生のバイブルはヴァイオレット・エヴァーガーデンとFate。
+      </div>
+   <div id="tabpage3">DISCOGRAPHY</div>
+    </div>
+        <p>
+          カウンタプログラム
+        </p>
+        <p>{counter}</p>
+        <button id="plus" type="button"   onClick={plusOne}>
+          +1する
+        </button>
+    </body>
+    </div>
+   
   );
 }
+export default App
 
-export default App;
+
